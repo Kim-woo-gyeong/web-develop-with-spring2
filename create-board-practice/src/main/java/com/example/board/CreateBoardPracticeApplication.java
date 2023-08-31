c9a654183ca8f8a8d0fc60db579489012a6c1a0f
김우경 <lovelykwg@naver.com>
2023-08-31 20:34:47 +0900

    #6 - SpringApplication.run 에 대한 주석추가
    인텔리제이 버그에 대한 설명 주석추가함.

--- create-board-practice/src/main/java/com/example/board/CreateBoardPracticeApplication.java
+++ create-board-practice/src/main/java/com/example/board/CreateBoardPracticeApplication.java
@@ -2,11 +2,14 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CreateBoardPracticeApplication {
 
     public static void main(String[] args) {
+        // 인텔리제이 이전 버전에서 run 이 주황밀줄이 쳐지는 버그가 발생했었음.
+        // 현재는 없어짐.
         SpringApplication.run(CreateBoardPracticeApplication.class, args);
     }
 
