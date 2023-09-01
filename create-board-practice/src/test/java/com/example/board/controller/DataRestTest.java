cdd71a7c96858ce3b0d8151c8c8301f45b7224b7
김우경 <lovelykwg@naver.com>
2023-09-01 15:20:06 +0900

    #8 - Data Rest Api 테스트 코드 작성

---
+++ create-board-practice/src/test/java/com/example/board/controller/DataRestTest.java
@@ -0,0 +1,45 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest - api 테스트")
+// webmvc 테스트는 컨트롤러 외의 build 를 load 하지 않음.
+// dataRest 를 읽지 않음!
+// STEP1.
+//@WebMvcTest
+//STEP2.
+@AutoConfigureMockMvc
+// 기본이 rollback
+@Transactional
+@SpringBootTest
+public class DataRestTest {
+    private final MockMvc mockMvc;
+
+    public DataRestTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("[api] 게시글 조회 테스트")
+    @Test
+    void apiTest() throws Exception {
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+        // application/hal+json : rest hal 의 형태랑 맞춰줌.
+    }
+}
