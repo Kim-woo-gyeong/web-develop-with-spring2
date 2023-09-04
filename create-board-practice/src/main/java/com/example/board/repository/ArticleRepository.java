c0e62bdc38b26489da96dca9aa62b18a17c2a306
김우경 <lovelykwg@naver.com>
2023-09-04 14:06:16 +0900

    #8 - QueryDsl debendency 추가
    QueryDsl 로 검색기능 구현하기

--- create-board-practice/src/main/java/com/example/board/repository/ArticleRepository.java
+++ create-board-practice/src/main/java/com/example/board/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package com.example.board.repository;
 
 import com.example.board.domain.Article;
+import com.example.board.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+         JpaRepository<Article, Long>
+        , QuerydslPredicateExecutor<Article>
+        , QuerydslBinderCustomizer<QArticle>
+{
+    // java8 부터 interface 를 직접 구현이 가능해짐.
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true); // 지정한 프로퍼티에 대해서만 검색되도록. defualt = false
+        bindings.including(root.title, root.content, root.hashTag, root.createdAt, root.createdBy);
+
+        // 이 경우에는 '%' 를 개발자가 직접 적용시켜줘야 함
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // ex) like '${vale}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // ex) like '%${vale}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashTag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    };
 }
